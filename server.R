dc3cb0f9238411a729dc563a0df43f4e5f66624b
John Lövrot <john.lovrot@ki.se>
2015-11-10 09:17:17 +0100

    Restructure R code

--- server.R
+++ server.R
@@ -10,7 +10,7 @@ function(input, output) {
 
     h <- 1.1
 
-    binoculars_view <- reactive({
+    derive_beta_angles <- reactive({
 
         alpha <- input$alpha_degrees/180*pi
 
@@ -20,24 +20,23 @@ function(input, output) {
         beta_1 <- atan(x_1/h)
         beta_2 <- atan(x_2/h)
 
-        df <- data.frame(
-            part=factor(names(palette_part), levels=names(palette_part)),
-            radians=c(2*pi - 2*beta_1, beta_1 + beta_2, beta_1 - beta_2))
-
-        attr(df,"beta_1") <- beta_1
-
-        return(df)
+        c(beta_1, beta_2)
     })
 
     output$plot <- renderPlot({
 
         theme_set(theme_bw())
 
-        df <- binoculars_view()
+        beta <- derive_beta_angles()
 
-        gg <- ggplot(data=df, aes(x=factor(1), y=radians, fill=part)) +
+        binoculars_view <- data.frame(
+            part=factor(names(palette_part), levels=names(palette_part)),
+            radians=c(2*pi - 2*beta[1], beta[1] + beta[2], beta[1] - beta[2]))
+
+        gg <- ggplot(data=binoculars_view,
+            aes(x=factor(1), y=radians, fill=part)) +
             geom_bar(width=1, stat="identity", position="fill") +
-            coord_polar(theta="y", start=pi + attr(df, "beta_1"), direction=-1) +
+            coord_polar(theta="y", start=pi + beta[1], direction=-1) +
             scale_x_discrete(breaks=NULL) +
             scale_y_continuous(breaks=NULL) +
             scale_fill_manual(values=palette_part) +
